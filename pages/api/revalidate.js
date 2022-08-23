d7cc3dc3811cf7f9bcf57f9578c38a651e759d1d
Matthew <matgold17@gmail.com>
2022-08-23 12:32:10 +0100

    updated

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    await res.unstable_revalidate('/');
+    await res.unstable_revalidate('/note/[id]');
     return res.json({ revalidated: true });
   } catch (error) {
     return res.status(500).send('Error revalidating');
