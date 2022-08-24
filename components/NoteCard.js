55ffa91d4439726949bb14ffcf4f868f8ea5b5a3
Matthew <matgold17@gmail.com>
2022-08-24 03:41:25 +0100

    updated

--- components/NoteCard.js
+++ components/NoteCard.js
@@ -5,11 +5,6 @@ import {
   Flex,
   Heading,
   HStack,
-  Popover,
-  PopoverContent,
-  PopoverHeader,
-  PopoverTrigger,
-  Portal,
   Spacer,
   Tag,
   Text,
@@ -32,71 +27,76 @@ function NoteCard({ note }) {
   const textColor = useColorModeValue('gray.800', 'gray.300');
 
   return (
-      <Box
-        pos='relative'
-        w='xs'
-        maxW='xs'
-        mx='auto'
-        border='1px'
-        borderRadius='md'
-        borderColor={useColorModeValue('gray.300', 'gray.700')}
-        overflow='hidden'
-        p='4'
-        _hover={{ boxShadow: 'lg' }}>
-        <Heading
-          textTransform='uppercase'
-          fontWeight='bold'
-          fontSize='2xl'
-          mt='3'
-          color={note.is_completed ? grayText : 'green'}>
-          {note?.title}
-        </Heading>
-        <Tag
-          pos='absolute'
-          top='3'
-          right='4'
-          borderRadius='3xl'
-          size='sm'
-          bg={note?.is_completed ? 'green.400' : 'yellow.400'}
-        />
-        <Flex justify='space-between'>
+    <Box
+      pos='relative'
+      w='xs'
+      maxW='xs'
+      mx='auto'
+      border='1px'
+      borderRadius='md'
+      borderColor={useColorModeValue('gray.300', 'gray.700')}
+      overflow='hidden'
+      p='4'
+      _hover={{ boxShadow: 'lg' }}>
+      <Heading
+        textTransform='uppercase'
+        fontWeight='bold'
+        fontSize='2xl'
+        mt='3'
+        color={note.is_completed ? grayText : 'green'}>
+        {note?.title}
+      </Heading>
+      <Tag
+        pos='absolute'
+        top='3'
+        right='4'
+        borderRadius='3xl'
+        size='sm'
+        bg={note?.is_completed ? 'green.400' : 'yellow.400'}
+      />
+      <Flex justify='space-between'>
+        <VStack spacing='-1' align='start'>
+          <Text fontSize='sm' color={grayText}>
+            Created
+          </Text>
+          <Text fontSize='sm' color={grayText}>
+            {dayjs(note.created_at).format('MMM D, YYYY')}
+          </Text>
+        </VStack>
+
+        {note.updated_on && (
           <VStack spacing='-1' align='start'>
             <Text fontSize='sm' color={grayText}>
-              Created
+              Updated
             </Text>
             <Text fontSize='sm' color={grayText}>
-              {dayjs(note.created_at).format('MMM D, YYYY')}
+              {dayjs(note.updated_on).fromNow()}
             </Text>
           </VStack>
-
-          {note.updated_on && (
-            <VStack spacing='-1' align='start'>
-              <Text fontSize='sm' color={grayText}>
-                Updated
-              </Text>
-              <Text fontSize='sm' color={grayText}>
-                {dayjs(note.updated_on).fromNow()}
-              </Text>
-            </VStack>
-          )}
-        </Flex>
-        <Divider my='4' />
-        <Text
-          fontSize='lg'
-          color={note.is_completed ? isCompletedTextColor : textColor}
-          noOfLines='2'>
-          {note.desc}
-        </Text>
-        <Divider my='4' />
-        <HStack>
-          <Spacer />
-          <Button
-          onClick={() => router.push('/note/[id]', `/note/${note.id}`)}
+        )}
+      </Flex>
+      <Divider my='4' />
+      <Text
+        fontSize='lg'
+        color={note.is_completed ? isCompletedTextColor : textColor}
+        noOfLines='2'>
+        {note.desc}
+      </Text>
+      <Divider my='4' />
+      <HStack>
+        <Spacer />
+        <Button
+          onClick={() =>
+            router.push({
+              pathname: `/note/[id]`,
+              query: { id: note.id },
+            })
+          }
           colorScheme='green'>
           {note.is_completed ? 'Edit/Remove' : 'See Details'}
         </Button>
-        </HStack>
-      </Box>
+      </HStack>
+    </Box>
   );
 }
 
