1026d6f9ae67a5f4cec9916409ad98b5cddc6392
Matthew <matgold17@gmail.com>
2022-08-25 21:53:32 +0100

    updated

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -15,6 +15,7 @@ import {
   Text,
   VStack,
 } from '@chakra-ui/react';
+import  Head  from 'next/head';
 import { useState } from 'react';
 import { BiMailSend } from 'react-icons/bi';
 import SigninForm from '../../components/SigninForm';
@@ -43,65 +44,80 @@ function Signin() {
   };
 
   return (
-    <Box w='100%' h='100vh'>
-      <Flex maxW='xs' mx='auto' mt='20' justify='center' flexDir='column'>
-        <Flex flexDir='row' justify='center'>
-          
-          <Heading justifyContent='center' fontSize='36'>JOTTA</Heading>
-        </Flex>
-        <Box bg='#45FC38' my='8' mx='auto' px='8' h='0.5' rounded='3xl'>
-          {/* <Divider border='2px' /> */}
-        </Box>
-        <Text textAlign='center' fontSize='xl'>
-          Save your notes, appointments and other schedules with JOTTA.
-        </Text>
-
-        {error && (
-          <Flex
-            mt='8'
-            p='6'
-            bg='red.100'
-            color='red'
-            justify='center'
-            align='center'
-            flexDir='column'
-            rounded='md'>
-            <WarningIcon fontSize='3xl' />
-
-            <Text textAlign='center' mt='4'>
-              {error.message}
-            </Text>
+    <div>
+      <Head>
+        <title>JOTTA | Sign in</title>
+        <meta
+          name='description'
+          content='JOTTA is an e jotter app for keeping notes, appointments and schedules.'
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Box w='100%' h='100vh'>
+        <Flex maxW='xs' mx='auto' mt='20' justify='center' flexDir='column'>
+          <Flex flexDir='row' justify='center'>
+            <Heading justifyContent='center' fontSize='36'>
+              JOTTA
+            </Heading>
           </Flex>
-        )}
+          <Box bg='#45FC38' my='8' mx='auto' px='8' h='0.5' rounded='3xl'>
+            {/* <Divider border='2px' /> */}
+          </Box>
+          <Text textAlign='center' fontSize='xl'>
+            Save your notes, appointments and other schedules with JOTTA.
+          </Text>
 
-        {isSubmitted ? (
-          <Flex
-            mt='12'
-            p='6'
-            bg='green.50'
-            color='green'
-            justify='center'
-            align='center'
-            flexDir='column'
-            rounded='md'>
-            <BiMailSend size='32' />
-            <Text textAlign='center' mt='2' fontWeight='semibold' fontSize='23'>
-              Check your email box
-            </Text>
-            <Text textAlign='center' mt='2' fontSize='18'>
-              Your login link has been sent to {email}
-            </Text>
-          </Flex>
-        ) : (
-          <SigninForm
+          {error && (
+            <Flex
+              mt='8'
+              p='6'
+              bg='red.100'
+              color='red'
+              justify='center'
+              align='center'
+              flexDir='column'
+              rounded='md'>
+              <WarningIcon fontSize='3xl' />
+
+              <Text textAlign='center' mt='4'>
+                {error.message}
+              </Text>
+            </Flex>
+          )}
+
+          {isSubmitted ? (
+            <Flex
+              mt='12'
+              p='6'
+              bg='green.50'
+              color='green'
+              justify='center'
+              align='center'
+              flexDir='column'
+              rounded='md'>
+              <BiMailSend size='32' />
+              <Text
+                textAlign='center'
+                mt='2'
+                fontWeight='semibold'
+                fontSize='23'>
+                Check your email box
+              </Text>
+              <Text textAlign='center' mt='2' fontSize='18'>
+                Your login link has been sent to {email}
+              </Text>
+            </Flex>
+          ) : (
+            <SigninForm
               email={email}
               setEmail={setEmail}
-            isLoading={isLoading}
-            handleSubmit={handleSubmit}
-          />
-        )}
-      </Flex>
-    </Box>
+              isLoading={isLoading}
+              handleSubmit={handleSubmit}
+            />
+          )}
+        </Flex>
+      </Box>
+    </div>
   );
 }
 
